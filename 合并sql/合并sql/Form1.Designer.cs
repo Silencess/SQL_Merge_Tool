5fd9c29b2121ddb1ad82f5d0591425a6b3f14e39
江雨寒 <1332657754@qq.com>
2019-03-10 17:16:39 +0800

    SQL合并小工具
    第一个版本，做备份

---
+++ 合并sql/合并sql/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace 合并sql
+{
+    partial class MergeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MergeForm));
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.mergeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(75, 31);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(203, 21);
+            this.textBox.TabIndex = 0;
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.Location = new System.Drawing.Point(300, 30);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(75, 21);
+            this.selectFileButton.TabIndex = 1;
+            this.selectFileButton.Text = "选择";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mergeButton
+            // 
+            this.mergeButton.Location = new System.Drawing.Point(300, 74);
+            this.mergeButton.Name = "mergeButton";
+            this.mergeButton.Size = new System.Drawing.Size(75, 21);
+            this.mergeButton.TabIndex = 2;
+            this.mergeButton.Text = "合并";
+            this.mergeButton.UseVisualStyleBackColor = true;
+            this.mergeButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "文件位置";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "编    码";
+            // 
+            // comboBox
+            // 
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "UTF-8",
+            "GBK"});
+            this.comboBox.Location = new System.Drawing.Point(75, 74);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(203, 20);
+            this.comboBox.TabIndex = 5;
+            // 
+            // MergeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 113);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mergeButton);
+            this.Controls.Add(this.selectFileButton);
+            this.Controls.Add(this.textBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MergeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL合并小工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button selectFileButton;
+        private System.Windows.Forms.Button mergeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox;
+    }
+}
+
---
+++ 合并sql/合并sql/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace 合并sql
+{
+    public partial class MergeForm : Form
+    {
+        string FileName = "";
+        public MergeForm()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    textBox.Text = dlg.SelectedPath;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            //设置文件类型 
+            sfd.Filter = "SQL文件（*.sql）|*.sql|文本文件（*.txt）|*.txt";
+
+            //设置默认文件类型显示顺序 
+            sfd.FilterIndex = 1;
+
+            //保存对话框是否记忆上次打开的目录 
+            sfd.RestoreDirectory = true;
+
+            //设置默认的文件名
+            sfd.FileName = DateTime.Now.ToString("yyyyMM");// in wpf is  sfd.FileName = "YourFileName";
+
+            //点了保存按钮进入 
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string localFilePath = sfd.FileName.ToString(); //获得文件路径 
+                string fileNameExt = localFilePath.Substring(localFilePath.LastIndexOf("\\") + 1); //获取文件名，不带路径
+                FileName = localFilePath;
+            }
+            else {
+                return;
+            }
+            //string FileName = @"D:\Update" + DateTime.Now.ToString("yyyyMM") + ".sql";
+            Thread waitT = new Thread(new ThreadStart(Progress));
+            if (textBox.Text == "")
+            {
+                MessageBox.Show("请选择文件后再合并！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            waitT.IsBackground = true;
+            waitT.Start();
+        }
+
+        public void Progress()
+        {
+            int dsa = comboBox.SelectedIndex;
+            try
+            {
+                FileInfo[] myFileInfo = (new DirectoryInfo(textBox.Text)).GetFiles("*.sql");
+                string NewFileName = "";
+                //空文件夹提示
+                if (myFileInfo.Length == 0) {
+                    MessageBox.Show("此文件夹内未找到SQL文件！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                for (int i = 0; i < myFileInfo.Length; i++)
+                {
+                    string str = textBox.Text + "\\" + myFileInfo[i].Name;
+                    StreamReader Strsw = new StreamReader(str, comboBox.SelectedIndex == 0 ? Encoding.UTF8 : Encoding.GetEncoding(54936));
+                    NewFileName += Strsw.ReadToEnd() + "\r\n\r\n /*******************分割线***************/ \r\n\r\n";
+                    Strsw.Close();
+                }
+                
+                FileStream myFs = null;
+                if (!File.Exists(FileName))
+                {
+                    myFs = new FileStream(FileName, FileMode.Create);
+                }
+                else
+                {
+                    myFs = new FileStream(FileName, FileMode.Open);
+                    MessageBox.Show("文件已存在，将会在原有基础上添加。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    StreamReader Strsw = new StreamReader(myFs, comboBox.SelectedIndex == 0 ? Encoding.UTF8 : Encoding.GetEncoding(54936));
+                    NewFileName = Strsw.ReadToEnd() + "\r\n\r\n /*******************分割线***************/ \r\n\r\n" + NewFileName;
+
+                }
+                StreamWriter sw = new StreamWriter(myFs, Encoding.UTF8);
+                sw.Write(NewFileName);
+                sw.Close();
+                MessageBox.Show("合并完毕，文件长度" + NewFileName.Length.ToString("n").Split('.')[0] + "KB大小", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
